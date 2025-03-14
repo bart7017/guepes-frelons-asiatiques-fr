95ef0264860855272a540abf8dfbfa0529deb685
elevaseo <75243836+bastienallain@users.noreply.github.com>
2025-03-14 10:48:04 +0700

    Default theme color

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,27 +1,27 @@
 export const siteConfig = {
-  companyName: 'Titan Core',
-  siteUrl: 'https://titan-core.com',
+  companyName: "Guepes frelons asiatiques",
+  siteUrl: "https://guepes-frelons-asiatiques.fr",
   Socials: {
-      xSocial: 'https://x.com/astrodotbuild',
-      Github: 'https://github.com/withastro',
-      Instagram: 'https://www.instagram.com/astrodotbuild',
-      LinkedIn: 'https://www.linkedin.com/company/astrodotbuild',
-      Email: 'support@titan-core.com',
-      Phone: '+123 456 7890',
-      Location: '123 Main St, Anytown, USA',
-  }
+    xSocial: "https://x.com/astrodotbuild",
+    Github: "https://github.com/withastro",
+    Instagram: "https://www.instagram.com/astrodotbuild",
+    LinkedIn: "https://www.linkedin.com/company/astrodotbuild",
+    Email: "support@titan-core.com",
+    Phone: "+123 456 7890",
+    Location: "123 Main St, Anytown, USA",
+  },
 };
 
 export const SEO = {
-  Separator: '|',
-  SiteName: 'Titan Core',
-  defaultDescription: 'A modern, high-performance Astro JS theme',
+  Separator: "|",
+  SiteName: "Titan Core",
+  defaultDescription: "A modern, high-performance Astro JS theme",
 };
-  
+
 export const blogSetting = {
-  postsPerPage: 6   
-}
+  postsPerPage: 6,
+};
 
 export const themeSetting = {
-  theme: 'zeus'
-}
+  theme: "artemis",
+};
